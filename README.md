dcbd181f50f2304d2809e4d53036a17b94b47f8a
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-08-16 09:40:42 -0400

    Add the xml_parser method

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 # Parse xml
 
+## I want to
+- Convert multiple different XML schemas into my standard schema.
+-
+
 ## Resources
+- [RubyConf 2014 - Rapidly Mapping JSON/XML API Schemas in Ruby by Adam Cuppy](
+https://www.youtube.com/watch?v=1K0Pt0o9F7w)
+- [4.1 MITS schema and an example](http://www.nmhc.org/content.aspx?id=8779)
 - [GemのいろいろXML/HTMLparserをまとめたい(Oga特集) - Qiita](http://qiita.com/gravitonMain/items/720f441713b1378fe55c#3-7)
 - [RubyでXMLファイルをHashとしてパースする方法の比較 - Qiita](http://qiita.com/tamano/items/4a6ea57bdb182a0bccce)
 - [Keeping attributes when converting XML to Ruby hash - Stack Overflow](http://stackoverflow.com/questions/19309465/keeping-attributes-when-converting-xml-to-ruby-hash)
 - [RubyでXMLに対応したクラスを扱うときのベストプラクティス - QA@IT](http://qa.atmarkit.co.jp/q/3447)
 - [Converting large XML documents to Ruby Hashes | Devnull](http://devnull.absolventa.de/2014/03/18/converting-large-xml-documents-to-ruby-hashes/)
+- [Oga](https://github.com/YorickPeterse/oga)
--- lib/xml_parser.rb
+++ lib/xml_parser.rb
@@ -2,19 +2,37 @@ require "rails/all"
 require "oga"
 require "awesome_print"
 
+# Parse the specified XML file.
+def xml_parser(filename)
+  # Read that file.
+  xml = File.read(filename)
+
+  # Parse xml using Oga.
+  doc = Oga.parse_xml(xml)
+end
+
 # Check current working directory.
 puts Dir.pwd
 
-# Filename in absolute path.
-filename = "/Users/masa/projects/ruby/parse_xml/db/feeds/ash_b.xml"
+# absolute path to the source XML file.
+filename = "/Users/mnishiguchi/projects/parse_xml/db/feeds/ash_a.xml"
+# filename = "/Users/masa/projects/ruby/parse_xml/db/feeds/ash_b.xml"
 p filename
 
-# Read that tile.
-xml = File.read(filename)
+# Parse the XML.
+doc = xml_parser(filename)
 
-# Parse xml
-doc = Oga.parse_xml(xml)
-
-# Inspect the result
+# Inspect the parsed document.
 # p doc
-p doc.xpath("//Property/Identification").map{ |t| t.text }
+# ap doc.xpath("//PhysicalProperty/Management").map{ |t| t.text }
+# ap doc.xpath("//PhysicalProperty/Property").map{ |t| t.text }
+# doc.xpath("//PhysicalProperty/Management").each do |item|
+#   puts item.get('name')
+# end
+doc.xpath("//PhysicalProperty/Management").each do |item|
+  # Read the attribute 'id' in the element.
+  ap item.get('id')
+
+  # Read the child element "<Name/>"
+  ap item.at_xpath('Name').text
+end
